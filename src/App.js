ded0599efb99caae312b4974c2941786d4219b7c
VanehSann <vanessacbsantos22@gmail.com>
2022-02-18 15:41:29 -0300

    requisito 01.header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 // dando commit
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (<Header />);
   }
 }
 
