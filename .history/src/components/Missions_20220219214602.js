ccdc17f7efed113709b6bbaf47afc8d8d2ba1469
VanehSann <vanessacbsantos22@gmail.com>
2022-02-19 21:48:51 -0300

    requisito 07

---
+++ .history/src/components/Missions_20220219214602.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from './Title';
+
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Planetas" />
+      </div>);
+  }
+}
+
+export default SolarSystem;
